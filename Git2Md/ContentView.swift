175bf3d16b7b8bb60c66c3be2cd9b40ee0bde553
Wayne Dahlberg <waynefj40@gmail.com>
2025-11-24 12:27:38 -0700

    Initial Commit

---
+++ Git2Md/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Git2Md
+//
+//  Created by Wayne Dahlberg on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Git2Md/Git2MdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git2MdApp.swift
+//  Git2Md
+//
+//  Created by Wayne Dahlberg on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Git2MdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
